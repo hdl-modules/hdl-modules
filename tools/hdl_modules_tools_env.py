d1b509901d1c21011cd35588ecab1aad27fefbf4
Lukas Vik <2767848-LukasVik@users.noreply.gitlab.com>
2021-09-22 11:37:02 +0200

    Update simulate.py to work in this repo

---
+++ tools/hdl_modules_tools_env.py
@@ -0,0 +1,6 @@
+from pathlib import Path
+
+REPO_ROOT = Path(__file__).parent.parent.resolve()
+HDL_MODULES_TEMP_DIR = REPO_ROOT / "generated"
+
+HDL_MODULES_MODULES_DIRECTORY = REPO_ROOT / "modules"
--- tools/simulate.py
+++ tools/simulate.py
@@ -7,25 +7,20 @@
 # --------------------------------------------------------------------------------------------------
 
 from pathlib import Path
-from shutil import which
 import sys
 
+import hdl_modules_tools_env
+
 # Do PYTHONPATH insert() instead of append() to prefer any local repo checkout over any pip install
-PATH_TO_TSFPGA = Path(__file__).parent.parent.resolve()
+PATH_TO_TSFPGA = hdl_modules_tools_env.REPO_ROOT.parent / "tsfpga"
 sys.path.insert(0, str(PATH_TO_TSFPGA))
-PATH_TO_VUNIT = PATH_TO_TSFPGA.parent / "vunit"
+PATH_TO_VUNIT = hdl_modules_tools_env.REPO_ROOT.parent / "vunit"
 sys.path.insert(0, str(PATH_TO_VUNIT))
 
 from vunit import VUnitCLI, VUnit
-from vunit.vivado.vivado import create_compile_order_file, add_from_compile_order_file
 
-import tsfpga
-import tsfpga.create_vhdl_ls_config
 from tsfpga.git_simulation_subset import GitSimulationSubset
-from tsfpga.vivado.ip_cores import VivadoIpCores
-from tsfpga.vivado.simlib import VivadoSimlib
-
-from examples.tsfpga_example_env import get_tsfpga_modules, TSFPGA_EXAMPLES_TEMP_DIR
+from tsfpga.module import get_modules
 
 
 def main():
@@ -50,16 +45,7 @@ def main():
         # Enable minimal compilation in VUnit
         args.minimal = True
 
-    vunit_proj, modules, ip_core_vivado_project_directory = setup_vunit_project(args)
-
-    create_vhdl_ls_configuration(
-        output_path=PATH_TO_TSFPGA,
-        vunit_proj=vunit_proj,
-        modules=modules,
-        ip_core_vivado_project_directory=ip_core_vivado_project_directory,
-    )
-
-    vunit_proj.main()
+    setup_vunit_project(args)
 
 
 def find_git_test_filters(args):
@@ -68,7 +54,7 @@ def find_git_test_filters(args):
     vunit_proj, modules, _ = setup_vunit_project(args)
 
     testbenches_to_run = GitSimulationSubset(
-        repo_root=tsfpga.REPO_ROOT,
+        repo_root=hdl_modules_tools_env.REPO_ROOT,
         reference_branch="origin/master",
         vunit_proj=vunit_proj,
         # We use VUnit preprocessing, so these arguments have to be supplied
@@ -90,40 +76,17 @@ def setup_vunit_project(args):
     vunit_proj.enable_location_preprocessing()
     vunit_proj.enable_check_preprocessing()
 
-    all_modules = get_tsfpga_modules()
-    has_commercial_simulator = vunit_proj.get_simulator_name() != "ghdl"
-
-    if has_commercial_simulator and not args.vivado_skip:
-        # Includes modules with IP cores. Can only be used with a commercial simulator.
-        sim_modules = all_modules
-    else:
-        # Only modules that do not contain IP cores
-        sim_modules = [module for module in all_modules if len(module.get_ip_core_files()) == 0]
-
-    if args.vivado_skip:
-        ip_core_vivado_project_directory = None
-    else:
-        add_simlib(vunit_proj, args.temp_dir, args.simlib_compile)
-
-        # Generate IP core simulation files. Will be used for the vhdl_ls config,
-        # even if they are not added to the simulation project.
-        (
-            ip_core_compile_order_file,
-            ip_core_vivado_project_directory,
-        ) = generate_ip_core_files(all_modules, args.temp_dir, args.ip_compile)
-        if has_commercial_simulator:
-            add_from_compile_order_file(vunit_proj, ip_core_compile_order_file)
-
-    for module in sim_modules:
+    for module in get_modules([hdl_modules_tools_env.HDL_MODULES_MODULES_DIRECTORY]):
         vunit_library = vunit_proj.add_library(module.library_name)
         for hdl_file in module.get_simulation_files():
             if hdl_file.is_vhdl or hdl_file.is_verilog_source:
                 vunit_library.add_source_file(hdl_file.path)
             else:
                 assert False, f"Can not handle this file: {hdl_file}"
+
         module.setup_vunit(vunit_proj)
 
-    return vunit_proj, all_modules, ip_core_vivado_project_directory
+    vunit_proj.main()
 
 
 def arguments():
@@ -131,17 +94,8 @@ def arguments():
     cli.parser.add_argument(
         "--temp-dir",
         type=Path,
-        default=TSFPGA_EXAMPLES_TEMP_DIR,
+        default=hdl_modules_tools_env.HDL_MODULES_TEMP_DIR,
         help="where to place files needed for simulation flow",
-    )
-    cli.parser.add_argument(
-        "--vivado-skip", action="store_true", help="skip all steps that require Vivado"
-    )
-    cli.parser.add_argument(
-        "--ip-compile", action="store_true", help="force (re)compile of IP cores"
-    )
-    cli.parser.add_argument(
-        "--simlib-compile", action="store_true", help="force (re)compile of Vivado simlib"
     )
     cli.parser.add_argument(
         "--git-minimal",
@@ -152,51 +106,6 @@ def arguments():
     args = cli.parse_args()
     args.output_path = args.temp_dir / "vunit_out"
     return args
-
-
-def add_simlib(vunit_proj, temp_dir, force_compile):
-    vivado_simlib = VivadoSimlib.init(temp_dir, vunit_proj)
-    if force_compile or vivado_simlib.compile_is_needed:
-        vivado_simlib.compile()
-        vivado_simlib.to_archive()
-    vivado_simlib.add_to_vunit_project()
-
-
-def generate_ip_core_files(modules, temp_dir, force_generate):
-    vivado_ip_cores = VivadoIpCores(modules, temp_dir, "xc7z020clg400-1")
-
-    if force_generate:
-        vivado_ip_cores.create_vivado_project()
-        vivado_project_created = True
-    else:
-        vivado_project_created = vivado_ip_cores.create_vivado_project_if_needed()
-
-    if vivado_project_created:
-        # If the IP core Vivado project has been (re)created we need to create
-        # a new compile order file
-        create_compile_order_file(
-            vivado_ip_cores.vivado_project_file, vivado_ip_cores.compile_order_file
-        )
-
-    return vivado_ip_cores.compile_order_file, vivado_ip_cores.project_directory
-
-
-def create_vhdl_ls_configuration(
-    output_path, vunit_proj, modules, ip_core_vivado_project_directory
-):
-    """
-    Create config for vhdl_ls. Granted this might no be the "correct" place for this functionality.
-    But since the call is somewhat quick (~10 ms), and simulate.py is run "often" it seems an
-    appropriate place in order to always have an up-to-date vhdl_ls config.
-    """
-    vivado_location = None if which("vivado") is None else Path(which("vivado"))
-    tsfpga.create_vhdl_ls_config.create_configuration(
-        output_path=output_path,
-        modules=modules,
-        vunit_proj=vunit_proj,
-        vivado_location=vivado_location,
-        ip_core_vivado_project_directory=ip_core_vivado_project_directory,
-    )
 
 
 if __name__ == "__main__":
