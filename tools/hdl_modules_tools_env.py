2898a8ef40068f36e11672231a39c699a2ef907c
Lukas Vik <2767848-LukasVik@users.noreply.gitlab.com>
2021-10-12 14:29:05 +0200

    Enable even more code sharing

--- tools/hdl_modules_tools_env.py
+++ tools/hdl_modules_tools_env.py
@@ -3,4 +3,4 @@ from pathlib import Path
 REPO_ROOT = Path(__file__).parent.parent.resolve()
 HDL_MODULES_TEMP_DIR = REPO_ROOT / "generated"
 
-HDL_MODULES_MODULES_DIRECTORY = REPO_ROOT / "modules"
+HDL_MODULES_DIRECTORY = REPO_ROOT / "modules"
--- tools/simulate.py
+++ tools/simulate.py
@@ -21,60 +21,41 @@ from vunit import VUnitCLI
 
 from tsfpga.module import get_modules
 
-import examples.simulate
+from examples.simulate import find_git_test_filters, get_arguments_cli, setup_vunit_project
 
 
 def main():
-    args = arguments()
+    cli = get_arguments_cli(default_output_path=hdl_modules_tools_env.HDL_MODULES_TEMP_DIR)
+    args = cli.parse_args()
 
-    modules = get_modules([hdl_modules_tools_env.HDL_MODULES_MODULES_DIRECTORY])
+    # Modules in this repo have no dependency on Vivado simlib or IP cores
+    args.vivado_skip = True
 
-    if args.git_minimal:
+    modules = get_modules([hdl_modules_tools_env.HDL_MODULES_DIRECTORY])
+
+    if args.vcs_minimal:
         if args.test_patterns != "*":
             sys.exit(
-                "Can not specify a test pattern when using the --git-minimal flag."
+                "Can not specify a test pattern when using the --vcs-minimal flag."
                 f" Got {args.test_patterns}",
             )
 
-        git_test_filters = examples.simulate.find_git_test_filters(
-            args=args, modules=modules, repo_root=hdl_modules_tools_env.REPO_ROOT
+        test_filters = find_git_test_filters(
+            args=args, repo_root=hdl_modules_tools_env.REPO_ROOT, modules=modules
         )
-        if not git_test_filters:
+        if not test_filters:
             print("Nothing to run. Appears to be no VHDL-related git diff.")
             return
 
         # Override the test pattern argument to VUnit
-        args.test_patterns = git_test_filters
+        args.test_patterns = test_filters
         print(f"Running VUnit with test pattern {args.test_patterns}")
 
         # Enable minimal compilation in VUnit
         args.minimal = True
 
-    vunit_proj, _ = examples.simulate.setup_vunit_project(args=args, modules=modules)
+    vunit_proj, _ = setup_vunit_project(args=args, modules=modules)
     vunit_proj.main()
-
-
-def arguments():
-    cli = VUnitCLI()
-    cli.parser.add_argument(
-        "--temp-dir",
-        type=Path,
-        default=hdl_modules_tools_env.HDL_MODULES_TEMP_DIR,
-        help="where to place files needed for simulation flow",
-    )
-    cli.parser.add_argument(
-        "--git-minimal",
-        action="store_true",
-        help="compile and run only a minimal set of tests based on git history",
-    )
-
-    args = cli.parse_args()
-    args.output_path = args.temp_dir / "vunit_out"
-
-    # Modules in this repo have no dependency on Vivado simlib or IP cores
-    args.vivado_skip = True
-
-    return args
 
 
 if __name__ == "__main__":
