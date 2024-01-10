c63e63f90c22dd37d7aafee3d0a9343ed1fa0a44
Lukas Vik <10241915+LukasVik@users.noreply.github.com>
2024-01-10 12:43:00 +0100

    Add AXI range checkers

---
+++ modules/axi/sim/axi_address_range_checker.vhd
@@ -0,0 +1,114 @@
+-- -------------------------------------------------------------------------------------------------
+-- Copyright (c) Lukas Vik. All rights reserved.
+--
+-- This file is part of the hdl-modules project, a collection of reusable, high-quality,
+-- peer-reviewed VHDL building blocks.
+-- https://hdl-modules.com
+-- https://github.com/hdl-modules/hdl-modules
+-- -------------------------------------------------------------------------------------------------
+-- Check that an AXI address channel does not produce transactions that are out of range.
+--
+-- .. warning::
+--
+--   This core checker is not suitable for instantiation in your design.
+--   Use :ref:`axi.axi_read_range_checker` or :ref:`axi.axi_write_range_checker` instead.
+-- -------------------------------------------------------------------------------------------------
+
+library ieee;
+use ieee.numeric_std.all;
+use ieee.std_logic_1164.all;
+
+library axi;
+use axi.axi_pkg.all;
+
+
+entity axi_address_range_checker is
+  generic (
+    address_width : positive range 1 to axi_a_addr_sz;
+    id_width : natural range 0 to axi_id_sz;
+    data_width : positive range 1 to axi_data_sz;
+    enable_axi3 : boolean;
+    supports_narrow_burst : boolean
+  );
+  port (
+    clk : in std_ulogic;
+    --# {{}}
+    address_m2s : in axi_m2s_a_t;
+    address_s2m : in axi_s2m_a_t
+  );
+end entity;
+
+architecture a of axi_address_range_checker is
+
+begin
+
+  ------------------------------------------------------------------------------
+  check_address : process
+    constant error_message : string := (
+      "Using address bit outside of "
+      & integer'image(address_width)
+      & "-bit range."
+    );
+  begin
+    wait until (address_s2m.ready and address_m2s.valid) = '1' and rising_edge(clk);
+
+    for bit_idx in address_width to address_m2s.addr'high loop
+      assert address_m2s.addr(bit_idx) /= '1' report error_message;
+    end loop;
+  end process;
+
+
+  ------------------------------------------------------------------------------
+  check_id : process
+    constant error_message : string := (
+      "Using ID bit outside of "
+      & integer'image(id_width)
+      & "-bit range."
+    );
+  begin
+    wait until (address_s2m.ready and address_m2s.valid) = '1' and rising_edge(clk);
+
+    for bit_idx in id_width to address_m2s.id'high loop
+      assert address_m2s.id(bit_idx) /= '1' report error_message;
+    end loop;
+  end process;
+
+
+  ------------------------------------------------------------------------------
+  check_size : process
+    constant natural_size : axi_a_size_t := to_size(data_width_bits=>data_width);
+    constant error_message : string := (
+      "Illegal transfer size. Natural size: "
+      & integer'image(to_integer(natural_size))
+      & "."
+    );
+  begin
+    wait until (address_s2m.ready and address_m2s.valid) = '1' and rising_edge(clk);
+
+    if supports_narrow_burst then
+      assert address_m2s.size <= natural_size report error_message;
+    else
+      assert address_m2s.size = natural_size report error_message;
+    end if;
+  end process;
+
+
+  ------------------------------------------------------------------------------
+  axi3_gen : if enable_axi3 generate
+
+    ------------------------------------------------------------------------------
+    check_len : process
+      constant arlen_width : positive := get_a_len_width(
+        max_burst_length_beats=>get_max_burst_length_beats(enable_axi3=>enable_axi3)
+      );
+      constant unused_arlen_zero : unsigned(axi_a_len_sz - 1 downto arlen_width) := (others => '0');
+    begin
+      wait until (address_s2m.ready and address_m2s.valid) = '1' and rising_edge(clk);
+
+      assert address_m2s.len(unused_arlen_zero'range) = unused_arlen_zero
+        report "Unused bits in AxLEN are not zero.";
+    end process;
+
+  end generate;
+
+end architecture;
---
+++ modules/axi/sim/axi_write_range_checker.vhd
@@ -0,0 +1,86 @@
+-- -------------------------------------------------------------------------------------------------
+-- Copyright (c) Lukas Vik. All rights reserved.
+--
+-- This file is part of the hdl-modules project, a collection of reusable, high-quality,
+-- peer-reviewed VHDL building blocks.
+-- https://hdl-modules.com
+-- https://github.com/hdl-modules/hdl-modules
+-- -------------------------------------------------------------------------------------------------
+-- Rudimentary simulation runtime checker that an AXI master does not produce transactions
+-- that are out of the range of a downstream memory slave.
+-- Suitable to instantiate at the end of your AXI chain, right before the AXI memory slave.
+--
+-- This entity is meant for simulation, but since it contains only quite simple assertions
+-- it should be no problem for a synthesis tool to strip it.
+-- However, it is probably a good idea to instantiate it within a simulation guard:
+--
+-- .. code-block:: vhdl
+--
+--   axi_range_checker_gen : if in_simulation generate
+--
+--     axi_write_range_checker_inst : entity work.axi_write_range_checker
+--       generic map (
+--         ...
+--       );
+--
+--   end generate;
+-- -------------------------------------------------------------------------------------------------
+
+library ieee;
+use ieee.std_logic_1164.all;
+
+library axi;
+use axi.axi_pkg.all;
+
+
+entity axi_write_range_checker is
+  generic (
+    address_width : positive range 1 to axi_a_addr_sz := axi_a_addr_sz;
+    id_width : natural range 0 to axi_id_sz := axi_id_sz;
+    data_width : positive range 1 to axi_data_sz := axi_data_sz;
+    enable_axi3 : boolean;
+    supports_narrow_burst : boolean
+  );
+  port (
+    clk : in std_ulogic;
+    --# {{}}
+    write_m2s : in axi_write_m2s_t;
+    write_s2m : in axi_write_s2m_t
+  );
+end entity;
+
+architecture a of axi_write_range_checker is
+
+begin
+
+  ------------------------------------------------------------------------------
+  axi_address_sanity_checker_inst : entity work.axi_address_range_checker
+    generic map (
+      address_width => address_width,
+      id_width => id_width,
+      data_width => data_width,
+      enable_axi3 => enable_axi3,
+      supports_narrow_burst => supports_narrow_burst
+    )
+    port map (
+      clk => clk,
+      --
+      address_m2s => write_m2s.aw,
+      address_s2m => write_s2m.aw
+    );
+
+
+  ------------------------------------------------------------------------------
+  check_strobe : process
+    constant strobe_width : positive := data_width / 8;
+    constant unused_strobe_zero : std_logic_vector(write_m2s.w.strb'high downto strobe_width) := (
+      others => '0'
+    );
+  begin
+    wait until (write_s2m.w.ready and write_m2s.w.valid) = '1' and rising_edge(clk);
+
+    assert write_m2s.w.strb(unused_strobe_zero'range) = unused_strobe_zero
+      report "Unused strobe bits must be zero";
+  end process;
+
+end architecture;
