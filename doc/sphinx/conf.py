e00a1abc4cfe1886f6fd92bcecab6c8d99038d0f
Lukas Vik <10241915+LukasVik@users.noreply.github.com>
2024-01-14 08:21:18 +0100

    Fix sitemap.xml URLs

--- doc/sphinx/conf.py
+++ doc/sphinx/conf.py
@@ -31,8 +31,12 @@ intersphinx_mapping = {
 
 symbolator_output_format = "png"
 
-# Base URL for generated sitemap XML
+# Base URL for generated sitemap.xml.
 html_baseurl = "https://hdl-modules.com"
+
+# To avoid "en" in the sitemap.xml URL.
+# https://sphinx-sitemap.readthedocs.io/en/latest/advanced-configuration.html
+sitemap_url_scheme = "{link}"
 
 # Include robots.txt which points to sitemap
 html_extra_path = ["robots.txt"]
