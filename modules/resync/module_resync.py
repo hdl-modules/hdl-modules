ca78f6806fbbc9e364b53d4b625842dde28fc4d5
Lukas Vik <10241915+LukasVik@users.noreply.github.com>
2024-01-22 09:14:05 +0100

    Minimize netlist imports

--- modules/resync/module_resync.py
+++ modules/resync/module_resync.py
@@ -71,7 +71,7 @@ class Module(BaseModule):
         from hdl_modules import get_hdl_modules
 
         projects = []
-        modules = get_hdl_modules()
+        modules = get_hdl_modules(names_include=[self.name, "common", "math"])
         part = "xc7z020clg400-1"
         generics = dict(width=16)
 
