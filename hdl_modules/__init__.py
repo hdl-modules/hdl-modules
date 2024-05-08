8725b14f23dab97f0c098a5738a6a45c6871cc38
Lukas Vik <10241915+LukasVik@users.noreply.github.com>
2024-05-08 07:20:00 +0200

    Release version 5.0.0

--- hdl_modules/__init__.py
+++ hdl_modules/__init__.py
@@ -20,7 +20,7 @@ if TYPE_CHECKING:
 
 REPO_ROOT = Path(__file__).parent.parent.resolve()
 
-__version__ = "4.0.1-dev"
+__version__ = "5.0.0"
 __doc__ = get_short_slogan()  # pylint: disable=redefined-builtin
 
 
