ca1ff3814191bbbe5ddd6f41cb115348c6214a6a
Lukas Vik <10241915+LukasVik@users.noreply.github.com>
2024-05-28 16:18:12 +0200

    Waive some CDC warnings that we know are safe

--- modules/fifo/scoped_constraints/asynchronous_fifo.tcl
+++ modules/fifo/scoped_constraints/asynchronous_fifo.tcl
@@ -7,8 +7,13 @@
 # https://github.com/hdl-modules/hdl-modules
 # --------------------------------------------------------------------------------------------------
 
-set clk_write [get_clocks -of_objects [get_ports clk_write]]
-set read_data [get_cells memory.memory_read_data_reg*]
+set clk_write [get_clocks -quiet -of_objects [get_ports "clk_write"]]
+set read_data [
+  get_cells \
+    -quiet \
+    -filter {PRIMITIVE_GROUP==FLOP_LATCH} \
+    "memory.memory_read_data_reg*"
+]
 
 # These registers exist as FFs when the RAM is implemented as distributed RAM (LUTRAM).
 # In this case there is a timing path from write clock to the read data registers which
@@ -17,9 +22,22 @@ set read_data [get_cells memory.memory_read_data_reg*]
 # See some old discussion in https://gitlab.com/tsfpga/tsfpga/merge_requests/20
 # This is also discussed in AMD UG903 and in various places in the forum.
 # In recent Vivado versions (at least 2023.2), the cells show up even when the RAM is implemented as
-# BRAM, but the constraint has no effect.
-# Hence it seems safe to apply it always.
+# BRAM, hence why we filter for the primitive type.
 if {${read_data} != "" && ${clk_write} != ""} {
   puts "INFO hdl-modules asynchronous_fifo.tcl: Setting false path to read data registers."
   set_false_path -setup -hold -from ${clk_write} -to ${read_data}
+
+  # Waive "LUTRAM read/write potential collision" warning to make reports a little cleaner.
+  # The 'report_cdc' command lists all the data bits as a warning, for example
+  # * From: asynchronous_fifo_inst/memory.mem_reg_0_15_0_5/RAMA_D1/CLK
+  # * To: asynchronous_fifo_inst/memory.memory_read_data_reg[0]
+  # The wildcards below aim to catch all these paths.
+  set cdc_from [get_pins -quiet "memory.mem_reg*/*/CLK"]
+  set cdc_to [get_pins -quiet "memory.memory_read_data_reg*/D"]
+  create_waiver \
+    -quiet \
+    -id "CDC-26" \
+    -from ${cdc_from} \
+    -to ${cdc_to} \
+    -description "Read/write pointer logic guarantees no collision"
 }
--- modules/resync/scoped_constraints/resync_counter.tcl
+++ modules/resync/scoped_constraints/resync_counter.tcl
@@ -11,13 +11,13 @@
 # Also AMD document UG903 provides some examples.
 # --------------------------------------------------------------------------------------------------
 
-set stable_registers [get_cells counter_in_gray_reg*]
-set first_resync_registers [get_cells counter_in_gray_p1_reg*]
+set stable_registers [get_cells "counter_in_gray_reg*"]
+set first_resync_registers [get_cells "counter_in_gray_p1_reg*"]
 
 # Try to find the period of the input clock.
 # Use either the actual value, or a safe default value (500 MHz => 2 ns) if the clock can
 # not be found at this stage.
-set clk_in [get_clocks -of_objects [get_ports clk_in]]
+set clk_in [get_clocks -quiet -of_objects [get_ports "clk_in"]]
 if {${clk_in} != ""} {
   set clk_in_period [get_property -min PERIOD ${clk_in}]
   puts "INFO hdl-modules resync_counter.tcl: Using clk_in period: ${clk_in_period}."
@@ -31,7 +31,7 @@ if {${clk_in} != ""} {
 set_bus_skew -from ${stable_registers} -to ${first_resync_registers} ${clk_in_period}
 
 # Try to find output clock period in a similar way.
-set clk_out [get_clocks -of_objects [get_ports clk_out]]
+set clk_out [get_clocks -quiet -of_objects [get_ports clk_out]]
 if {${clk_out} != ""} {
   set clk_out_period [get_property -min PERIOD ${clk_out}]
   puts "INFO hdl-modules resync_counter.tcl: Using clk_out period: ${clk_out_period}."
@@ -52,3 +52,17 @@ set min_period [expr {min(${clk_in_period}, ${clk_out_period})}]
 # Typically happens when a derived clock or a clock from an IP core is used.
 # Hence we use the flag.
 set_max_delay -datapath_only -from ${stable_registers} -to ${first_resync_registers} ${min_period}
+
+# Waive "Multi-bit synchronized with ASYNC_REG property" warning to make reports a little cleaner.
+# The 'report_cdc' command lists all the data bits as a warning, for example
+# * From: resync_counter_inst/counter_in_gray_reg[0]/C
+# * To: resync_counter_inst/counter_in_gray_p1_reg[0]/D
+# The wildcards below aim to catch all these paths.
+set cdc_from [get_pins -quiet "counter_in_gray_reg*/C"]
+set cdc_to [get_pins -quiet "counter_in_gray_p1_reg*/D"]
+create_waiver \
+  -quiet \
+  -id "CDC-6" \
+  -from ${cdc_from} \
+  -to ${cdc_to} \
+  -description "Multi-bit resynchronization safe since Grey code and proper constraints are used"
