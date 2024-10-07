57ff85576cd9d0e3b0ab3f583a68701604f2c585
Lukas Vik <10241915+LukasVik@users.noreply.github.com>
2024-10-07 16:31:26 +0200

    Add alternative simple_dma simulation method

--- modules/simple_dma/sim/simple_dma_sim_pkg.vhd
+++ modules/simple_dma/sim/simple_dma_sim_pkg.vhd
@@ -17,6 +17,7 @@ use vunit_lib.check_pkg.all;
 use vunit_lib.com_types_pkg.network_t;
 use vunit_lib.integer_array_pkg.all;
 use vunit_lib.memory_pkg.all;
+use vunit_lib.queue_pkg.all;
 
 library common;
 use common.addr_pkg.all;
@@ -27,6 +28,20 @@ use work.simple_dma_register_read_write_pkg.all;
 
 package simple_dma_sim_pkg is
 
+  -- Run a test where the data written to memory by the DUT is pushed byte-by-byte to a queue.
+  -- No verification of data is done so that must be done outside of the procedure.
+  procedure run_simple_dma_test(
+    signal net : inout network_t;
+    constant receive_num_bytes : positive;
+    constant receive_data_queue : queue_t;
+    buffer_size_bytes : in positive;
+    buffer_alignment : in positive;
+    memory : in memory_t;
+    regs_base_address : in addr_t := (others => '0')
+  );
+
+  -- Run a test where the data written to memory by the DUT is verified byte-by-byte to
+  -- reference data.
   procedure run_simple_dma_test(
     signal net : inout network_t;
     reference_data : in integer_array_t;
@@ -42,16 +57,16 @@ package body simple_dma_sim_pkg is
 
   procedure run_simple_dma_test(
     signal net : inout network_t;
-    reference_data : in integer_array_t;
+    constant receive_num_bytes : positive;
+    constant receive_data_queue : queue_t;
     buffer_size_bytes : in positive;
     buffer_alignment : in positive;
     memory : in memory_t;
     regs_base_address : in addr_t := (others => '0')
   ) is
     variable buf : buffer_t := null_buffer;
-    constant test_data_num_bytes : positive := length(reference_data);
 
-    variable write_address, read_address, num_bytes_checked : natural := 0;
+    variable write_address, read_address, num_bytes_pushed : natural := 0;
   begin
     buf := allocate(
       memory => memory,
@@ -80,21 +95,14 @@ package body simple_dma_sim_pkg is
 
     write_simple_dma_config(net=>net, value=>(enable=>'1'), base_address=>regs_base_address);
 
-    while num_bytes_checked /= test_data_num_bytes loop
+    while num_bytes_pushed /= receive_num_bytes loop
       read_simple_dma_buffer_written_address(
         net=>net, value=>write_address, base_address=>regs_base_address
       );
 
       while write_address /= read_address loop
-        check_equal(
-          read_byte(memory=>memory, address=>read_address),
-          get(arr=>reference_data, idx=>num_bytes_checked),
-          "num_bytes_checked: "
-          & to_string(num_bytes_checked)
-          & ", read_address: " & to_string(read_address)
-        );
-
-        num_bytes_checked := num_bytes_checked + 1;
+        push(receive_data_queue, read_byte(memory=>memory, address=>read_address));
+        num_bytes_pushed := num_bytes_pushed + 1;
 
         if read_address = last_address(buf) then
           read_address := base_address(buf);
@@ -107,6 +115,40 @@ package body simple_dma_sim_pkg is
         net=>net, value=>read_address, base_address=>regs_base_address
       );
     end loop;
+  end procedure;
+
+
+  procedure run_simple_dma_test(
+    signal net : inout network_t;
+    reference_data : in integer_array_t;
+    buffer_size_bytes : in positive;
+    buffer_alignment : in positive;
+    memory : in memory_t;
+    regs_base_address : in addr_t := (others => '0')
+  ) is
+    constant receive_num_bytes : positive := length(reference_data);
+    constant receive_data_queue : queue_t := new_queue;
+  begin
+    run_simple_dma_test(
+      net=>net,
+      receive_num_bytes=>receive_num_bytes,
+      receive_data_queue=>receive_data_queue,
+      buffer_size_bytes=>buffer_size_bytes,
+      buffer_alignment=>buffer_alignment,
+      memory=>memory,
+      regs_base_address=>regs_base_address
+    );
+
+    for check_byte_idx in 0 to receive_num_bytes - 1 loop
+      check_equal(
+        pop_integer(receive_data_queue),
+        get(arr=>reference_data, idx=>check_byte_idx),
+        "check_byte_idx: "
+        & to_string(check_byte_idx)
+      );
+    end loop;
+
+    check_true(is_empty(receive_data_queue), "Got more data than expected");
   end procedure;
 
 end package body;
