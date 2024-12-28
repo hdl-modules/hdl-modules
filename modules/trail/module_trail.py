59509c9e3d7768e2cec834d2f4851ac1593b1302
Lukas Vik <10241915+LukasVik@users.noreply.github.com>
2024-12-28 14:29:32 +0100

    Add axi_lite_to_trail.vhd

--- modules/trail/module_trail.py
+++ modules/trail/module_trail.py
@@ -23,6 +23,10 @@ class Module(BaseModule):
 
         tb_trail_pipeline = vunit_proj.library(self.library_name).test_bench("tb_trail_pipeline")
 
+        tb_axi_lite_to_trail = vunit_proj.library(self.library_name).test_bench(
+            "tb_axi_lite_to_trail"
+        )
+
         for data_width in [8, 16, 32, 64]:
             for address_width in [7, 24, 40]:
                 generics = dict(address_width=address_width, data_width=data_width)
@@ -34,6 +38,15 @@ class Module(BaseModule):
                     test=tb_trail_pipeline, generics=generics, set_random_seed=True
                 )
 
+                if data_width in [32, 64] and address_width < 32:
+                    for _ in range(4):
+                        self.add_vunit_config(
+                            test=tb_axi_lite_to_trail, generics=generics, set_random_seed=True
+                        )
+                    self.add_vunit_config(
+                        test=tb_axi_lite_to_trail, generics=generics, set_random_seed=711
+                    )
+
     def get_build_projects(self):
         # The 'hdl_modules' Python package is probably not on the PYTHONPATH in most scenarios where
         # this module is used. Hence we can not import at the top of this file.
@@ -44,20 +57,25 @@ class Module(BaseModule):
         from hdl_modules import get_hdl_modules
 
         projects = []
-        all_modules = get_hdl_modules(names_include=[self.name, "common", "math"])
+        all_modules = get_hdl_modules(
+            names_include=[self.name, "axi", "axi_lite", "common", "math"]
+        )
         part = "xc7z020clg400-1"
 
-        def add_trail_pipeline(generics: dict[str, bool], ffs: int):
+        def add(name: str, generics: dict, luts: int, ffs: int):
             projects.append(
                 TsfpgaExampleVivadoNetlistProject(
-                    name=self.test_case_name(name="trail_pipeline", generics=generics),
+                    name=self.test_case_name(name=name, generics=generics),
                     modules=all_modules,
                     part=part,
-                    top="trail_pipeline",
+                    top=name,
                     generics=generics,
-                    build_result_checkers=[TotalLuts(EqualTo(0)), Ffs(EqualTo(ffs))],
+                    build_result_checkers=[TotalLuts(EqualTo(luts)), Ffs(EqualTo(ffs))],
                 )
             )
+
+        def add_trail_pipeline(generics: dict[str, bool], ffs: int):
+            add(name="trail_pipeline", generics=generics, luts=0, ffs=ffs)
 
         add_trail_pipeline(generics=dict(address_width=38, data_width=64), ffs=0)
         add_trail_pipeline(
@@ -82,6 +100,19 @@ class Module(BaseModule):
                 pipeline_response_read_data=True,
             ),
             ffs=53,
+        )
+
+        add(
+            name="axi_lite_to_trail",
+            generics=dict(address_width=24, data_width=32),
+            luts=29,
+            ffs=64,
+        )
+        add(
+            name="axi_lite_to_trail",
+            generics=dict(address_width=35, data_width=64),
+            luts=34,
+            ffs=106,
         )
 
         return projects
