8771ba36f4a5038f906c7f74b16b9395b8a7ce5d
Lukas Vik <10241915+LukasVik@users.noreply.github.com>
2025-02-15 11:37:29 +0100

    Add round_signed.vhd

--- modules/math/module_math.py
+++ modules/math/module_math.py
@@ -11,6 +11,14 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING, Any
 
+from tsfpga.examples.vivado.project import TsfpgaExampleVivadoNetlistProject
+from tsfpga.module import BaseModule
+from tsfpga.vivado.build_result_checker import (
+    EqualTo,
+    Ffs,
+    MaximumLogicLevel,
+    TotalLuts
+)
 from tsfpga.module import BaseModule
 
 if TYPE_CHECKING:
@@ -31,3 +39,33 @@ class Module(BaseModule):
                     name=name,
                     generics={"dividend_width": dividend_width, "divisor_width": divisor_width},
                 )
+
+    def get_build_projects(self) -> list[TsfpgaExampleVivadoNetlistProject]:
+        # The 'hdl_modules' Python package is probably not on the PYTHONPATH in most scenarios where
+        # this module is used. Hence we can not import at the top of this file.
+        # This method is only called when running netlist builds in the hdl-modules repo from the
+        # bundled tools/build_fpga.py, where PYTHONPATH is correctly set up.
+        from hdl_modules import get_hdl_modules
+
+        projects = []
+        all_modules = get_hdl_modules(
+            names_include=[self.name,  "common"]
+        )
+        part = "xc7z020clg400-1"
+
+        projects.append(
+            TsfpgaExampleVivadoNetlistProject(
+                name=f"{self.library_name}.round_signed",
+                modules=all_modules,
+                top="round_signed",
+                part=part,
+                generics = {"input_width": 32, "result_width": 24, "enable_output_register":True},
+                build_result_checkers=[
+                    TotalLuts(EqualTo(6)),
+                    Ffs(EqualTo(25)),
+                    MaximumLogicLevel(EqualTo(6)),
+                ],
+            )
+        )
+
+        return projects
---
+++ modules/math/src/round_signed.vhd
@@ -0,0 +1,103 @@
+-- -------------------------------------------------------------------------------------------------
+-- Copyright (c) Lukas Vik. All rights reserved.
+--
+-- This file is part of the hdl-modules project, a collection of reusable, high-quality,
+-- peer-reviewed VHDL building blocks.
+-- https://hdl-modules.com
+-- https://github.com/hdl-modules/hdl-modules
+-- -------------------------------------------------------------------------------------------------
+-- TODO
+-- -------------------------------------------------------------------------------------------------
+
+library ieee;
+use ieee.numeric_std.all;
+use ieee.std_logic_1164.all;
+
+library common;
+use common.types_pkg.all;
+
+use work.math_pkg.all;
+
+
+entity round_signed is
+  generic (
+    input_width : positive;
+    result_width : positive;
+    enable_output_register : boolean := false
+  );
+  port(
+    clk : in std_ulogic;
+    --# {{}}
+    input_value : in u_signed(input_width - 1 downto 0);
+    --# {{}}
+    result_value : out u_signed(result_width - 1 downto 0) := (others => '0');
+    result_is_saturated : out std_ulogic := '0'
+  );
+end entity;
+
+architecture a of round_signed is
+
+  signal result : u_signed(result_value'range) := (others => '0');
+  signal is_saturated : std_ulogic := '0';
+
+begin
+
+  assert result_width <= input_width
+    report "We can not make the number wider"
+    severity failure;
+
+
+  ------------------------------------------------------------------------------
+  passthrough_or_not_gen : if input_width = result_width generate
+
+    result <= input_value;
+
+
+  ------------------------------------------------------------------------------
+  else generate
+    constant max_result : signed(result_width - 1 downto 0) := get_max_signed(
+      num_bits=>result_width
+    );
+
+    constant num_lsb_to_remove : positive := input_width - result_width;
+    constant point_five_index : natural := num_lsb_to_remove - 1;
+
+    signal input_value_truncated : signed(result_width - 1 downto 0) := (others => '0');
+    signal point_five : natural range 0 to 1 := 0;
+  begin
+
+    input_value_truncated <= input_value(input_value'high downto num_lsb_to_remove);
+    point_five <= to_int(input_value(point_five_index));
+
+
+    ------------------------------------------------------------------------------
+    assign : process(all)
+    begin
+      if input_value_truncated = max_result and point_five = 1 then
+        result <= max_result;
+        is_saturated <= '1';
+      else
+        result <= input_value_truncated + point_five;
+        is_saturated <= '0';
+      end if;
+    end process;
+
+  end generate;
+
+
+  ------------------------------------------------------------------------------
+  output_register_gen : if enable_output_register generate
+
+    result_value <= result when rising_edge(clk);
+    result_is_saturated <= is_saturated when rising_edge(clk);
+
+
+  ------------------------------------------------------------------------------
+  else generate
+
+    result_value <= result;
+    result_is_saturated <= is_saturated;
+
+  end generate;
+
+end architecture;
