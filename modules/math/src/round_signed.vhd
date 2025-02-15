d786121db1e66677e541256ea872caa386cb644d
Lukas Vik <10241915+LukasVik@users.noreply.github.com>
2025-02-15 11:42:37 +0100

    DROPME, shit commit: Implement alternative

--- modules/math/src/round_signed.vhd
+++ modules/math/src/round_signed.vhd
@@ -37,8 +37,8 @@ end entity;
 
 architecture a of round_signed is
 
-  signal result : u_signed(result_value'range) := (others => '0');
-  signal is_saturated : std_ulogic := '0';
+  -- signal result : u_signed(result_value'range) := (others => '0');
+  -- signal is_saturated : std_ulogic := '0';
 
 begin
 
@@ -50,54 +50,50 @@ begin
   ------------------------------------------------------------------------------
   passthrough_or_not_gen : if input_width = result_width generate
 
-    result <= input_value;
+    -- result <= input_value;
 
 
   ------------------------------------------------------------------------------
   else generate
-    constant max_result : signed(result_width - 1 downto 0) := get_max_signed(
-      num_bits=>result_width
-    );
+    -- constant max_result : signed(result_width - 1 downto 0) := get_max_signed(
+    --   num_bits=>result_width
+    -- );
 
     constant num_lsb_to_remove : positive := input_width - result_width;
     constant point_five_index : natural := num_lsb_to_remove - 1;
 
     signal input_value_truncated : signed(result_width - 1 downto 0) := (others => '0');
+    signal input_value_extended, result_extended : signed(
+      input_value_truncated'length downto 0
+    ) := (others => '0');
+
     signal point_five : natural range 0 to 1 := 0;
   begin
 
     input_value_truncated <= input_value(input_value'high downto num_lsb_to_remove);
     point_five <= to_int(input_value(point_five_index));
 
+    input_value_extended <= resize(input_value_truncated, result_value'length + 1);
+    result_extended <= input_value_extended + point_five;
+
 
     ------------------------------------------------------------------------------
-    assign : process(all)
-    begin
-      if input_value_truncated = max_result and point_five = 1 then
-        result <= max_result;
-        is_saturated <= '1';
-      else
-        result <= input_value_truncated + point_five;
-        is_saturated <= '0';
-      end if;
-    end process;
+    saturate_signed_inst : entity work.saturate_signed
+      generic map(
+        input_width => result_extended'length,
+        result_width => result_value'length,
+        enable_output_register => enable_output_register
+      )
+      port map(
+        clk => clk,
+        --
+        input_value => result_extended,
+        --
+        result_value => result_value,
+        result_is_saturated => result_is_saturated
+      );
 
   end generate;
 
-
-  ------------------------------------------------------------------------------
-  output_register_gen : if enable_output_register generate
-
-    result_value <= result when rising_edge(clk);
-    result_is_saturated <= is_saturated when rising_edge(clk);
-
-
-  ------------------------------------------------------------------------------
-  else generate
-
-    result_value <= result;
-    result_is_saturated <= is_saturated;
-
-  end generate;
 
 end architecture;
