a76b541607e1907c669738811628ddc18488e9be
Lukas Vik <10241915+LukasVik@users.noreply.github.com>
2025-05-09 08:41:16 +0200

    BFM review fixes

--- modules/bfm/sim/axi_lite_master_bfm.vhd
+++ modules/bfm/sim/axi_lite_master_bfm.vhd
@@ -28,7 +28,6 @@
 -- See e.g. the file ``tb_axi_lite_register_file.vhd`` for a usage example.
 --
 --
---
 -- Randomization
 -- _____________
 --
@@ -113,6 +112,9 @@ entity axi_lite_master_bfm is
   );
   port (
     clk : in std_ulogic;
+    -- Synchronous reset of the BFM.
+    -- Will cancel any ongoing transactions, and clear the queue of upcoming operations.
+    -- Initial reset is NOT needed, the port port can safely be left unconnected and tied to zero.
     reset : in std_ulogic := '0';
     --# {{}}
     axi_lite_m2s : out axi_lite_m2s_t := axi_lite_m2s_init;
@@ -123,16 +125,17 @@ end entity;
 architecture a of axi_lite_master_bfm is
 
   subtype address_range is natural range address_length(bus_handle) - 1 downto 0;
-  subtype data_range is natural range data_length(bus_handle) - 1 downto 0;
+
+  constant data_width : positive := data_length(bus_handle);
+  subtype data_range is natural range data_width - 1 downto 0;
 
   type queues_t is record
     address : queue_t;
     data : queue_t;
-    response : queue_t;
+    response_code : queue_t;
     response_address : queue_t;
     reply : queue_t;
   end record;
-
   constant read_queues, write_queues : queues_t := (others => new_queue);
 
 begin
@@ -146,7 +149,7 @@ begin
     begin
       push(queues.address, address);
       push(queues.data, pop_std_ulogic_vector(request_msg));
-      push(queues.response, pop_std_ulogic_vector(request_msg));
+      push(queues.response_code, pop_std_ulogic_vector(request_msg));
       push(queues.response_address, address);
 
       push(queues.reply, request_msg);
@@ -154,6 +157,8 @@ begin
 
     impure function is_idle return boolean is
     begin
+      -- Popping the 'reply' queue is the last thing that happens in both the read and write
+      -- processes. Hence it is a suitable way knowing if we are idle.
       return is_empty(read_queues.reply) and is_empty(write_queues.reply);
     end function;
 
@@ -170,9 +175,9 @@ begin
       push_operation(queues=>write_queues, request_msg=>request_msg);
 
     elsif msg_type = wait_until_idle_msg then
-      if not is_idle then
-        wait until is_idle and rising_edge(clk);
-      end if;
+      while not is_idle loop
+        wait until rising_edge(clk);
+      end loop;
       handle_wait_until_idle(net, msg_type, request_msg);
 
     else
@@ -189,7 +194,9 @@ begin
     ------------------------------------------------------------------------------
     ar_block : block
       signal data_is_valid : std_ulogic := '0';
-      signal addr_target : u_unsigned(address_range) := (others => '0');
+      signal addr_target : u_unsigned(axi_lite_m2s.read.ar.addr'range) := (
+        others => drive_invalid_value
+      );
     begin
 
       ------------------------------------------------------------------------------
@@ -202,7 +209,7 @@ begin
         if reset then
           flush(read_queues.address);
         else
-          addr_target <= pop(read_queues.address);
+          addr_target(address_range) <= pop(read_queues.address);
           data_is_valid <= '1';
 
           wait until (
@@ -230,7 +237,7 @@ begin
         );
 
       -- Set the bus members only when bus is valid.
-      axi_lite_m2s.read.ar.addr(addr_target'range) <=
+      axi_lite_m2s.read.ar.addr <=
         addr_target when axi_lite_m2s.read.ar.valid else (others => drive_invalid_value);
 
 
@@ -250,27 +257,33 @@ begin
 
     ------------------------------------------------------------------------------
     r_block : block
-      constant data_width : positive := data_length(bus_handle);
       alias got_data is axi_lite_s2m.read.r.data(data_width - 1 downto 0);
+      signal read_count : natural := 0;
     begin
 
       ------------------------------------------------------------------------------
       check_r : process
-        function get_message(name : string) return string is
+        variable read_address : u_unsigned(address_range) := (others => '0');
+
+        impure function get_message(name : string) return string is
         begin
           return (
             "axi_lite_read_master"
             & logger_name_suffix
-            & " - R - '"
+            & " - R - "
+            & "operation #"
+            & to_string(read_count)
+            & " - '"
             & name
-            & "' mismatch when reading address "
+            & "' mismatch when reading address 0x"
+            & to_hstring(read_address)
+            & " ("
+            & to_string(to_integer(read_address))
+            & ")"
           );
         end function;
-        constant resp_message : string := get_message("resp");
-        constant data_message : string := get_message("data");
 
-        variable read_address : u_unsigned(address_range) := (others => '0');
-        variable expected_data : std_ulogic_vector(got_data'range) := (others => '0');
+        variable expected_data : std_ulogic_vector(data_range) := (others => '0');
 
         variable request_msg, reply_msg : msg_t := null_msg;
       begin
@@ -281,7 +294,7 @@ begin
 
         if reset then
           flush(read_queues.data);
-          flush(read_queues.response);
+          flush(read_queues.response_code);
           flush(read_queues.response_address);
           flush(read_queues.reply);
         else
@@ -289,16 +302,16 @@ begin
 
           check_equal(
             axi_lite_s2m.read.r.resp,
-            pop_std_ulogic_vector(read_queues.response),
-            resp_message & to_string(read_address)
+            pop_std_ulogic_vector(read_queues.response_code),
+            get_message(name=>"resp")
           );
 
           expected_data := pop(read_queues.data);
           if axi_lite_s2m.read.r.resp = axi_lite_resp_okay then
-            check_equal(
-              got_data, expected_data, data_message & to_string(read_address)
-            );
+            check_equal(got_data, expected_data, get_message(name=>"data"));
           end if;
+
+          read_count <= read_count + 1;
 
           request_msg := pop(read_queues.reply);
           reply_msg := new_msg;
@@ -419,7 +432,7 @@ begin
         others => drive_invalid_value
       );
       constant strb_target : std_ulogic_vector(axi_lite_m2s.write.w.strb'range) := to_axi_lite_strb(
-        data_width=>data_length(bus_handle)
+        data_width=>data_width
       );
     begin
 
@@ -484,17 +497,29 @@ begin
 
     ------------------------------------------------------------------------------
     b_block : block
+      signal write_count : natural := 0;
     begin
 
       ------------------------------------------------------------------------------
       check_b : process
-        constant resp_message : string := (
-          "axi_lite_read_master"
-          & logger_name_suffix
-          & " - B - 'resp' mismatch when writing address "
-        );
-
         variable write_address : u_unsigned(address_range) := (others => '0');
+
+        impure function get_message return string is
+        begin
+          return (
+            "axi_lite_read_master"
+            & logger_name_suffix
+            & " - B - "
+            & "operation #"
+            & to_string(write_count)
+            & " - 'resp' mismatch when writing address 0x"
+            & to_hstring(write_address)
+            & " ("
+            & to_string(to_integer(write_address))
+            & ")"
+          );
+        end function;
+
         variable request_msg, reply_msg : msg_t := null_msg;
       begin
         wait until (
@@ -503,7 +528,7 @@ begin
         );
 
         if reset then
-          flush(write_queues.response);
+          flush(write_queues.response_code);
           flush(write_queues.response_address);
           flush(write_queues.reply);
         else
@@ -511,9 +536,11 @@ begin
 
           check_equal(
             axi_lite_s2m.write.b.resp,
-            pop_std_ulogic_vector(write_queues.response),
-            resp_message & to_string(write_address)
+            pop_std_ulogic_vector(write_queues.response_code),
+            get_message
           );
+
+          write_count <= write_count + 1;
 
           request_msg := pop(write_queues.reply);
           reply_msg := new_msg;
