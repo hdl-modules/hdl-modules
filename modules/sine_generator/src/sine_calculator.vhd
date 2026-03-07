2161f6cde157150ef8d27fab911b3ca9ecaffcec
Lukas Vik <10241915+LukasVik@users.noreply.github.com>
2026-03-07 11:47:25 +1100

    Fix sphinx HTML math rendering
    Closes #207

--- modules/sine_generator/src/sine_calculator.vhd
+++ modules/sine_generator/src/sine_calculator.vhd
@@ -73,7 +73,7 @@
 --
 -- .. math::
 --
---   B \equiv \frac{\pi / 2}{2^\text{memory_address_width}}.
+--   B \equiv \frac{\pi / 2}{2^\text{memory\_address\_width}}.
 --
 -- The calculation is partitioned like this, using DSP48 blocks:
 --
